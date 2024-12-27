22be76a94faa8a37e0661a80d16dc4ebdd14d78f
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-12-27 06:45:46 -0500

    Update show.blade.php
    LOS CAMBIOS QUE AL SEÑOR BRASD NO LE GUSTA HACER EN EL REPOSITORIO .. HARAGAN

--- resources/views/rss/show.blade.php
+++ resources/views/rss/show.blade.php
@@ -1,5 +1,6 @@
 @php
     echo '<?xml version="1.0" encoding="UTF-8" ?>';
+    $poster='';
 @endphp
 <rss version="2.0"
      xmlns:atom="http://www.w3.org/2005/Atom"
@@ -19,14 +20,53 @@
         <ttl>5</ttl>
         @if($torrents)
             @foreach($torrents as $torrent)
+                @php
+                        $meta = match (true) {
+                            $torrent['category']['tv_meta'] => App\Models\Tv::query()
+                                ->with('genres', 'networks', 'seasons')
+                                ->find($torrent['tmdb'] ?? 0),
+                            $torrent['category']['movie_meta'] => App\Models\Movie::query()
+                                ->with('genres', 'companies', 'collection')
+                                ->find($torrent['tmdb'] ?? 0),                            
+                            default => null,
+                        };
+                 @endphp
                 <item>
                     <title>{{ $torrent['name'] }}</title>
                     <category>{{ $torrent['category']['name'] }}</category>
                     <contentlength>{{ $torrent['size'] }}</contentlength>
-                    <link>{{ route('torrent.download.rsskey', ['id' => $torrent['id'], 'rsskey' => $user->rsskey ]) }}</link>
+                    <type>{{ $torrent['type']['name'] }}</type> 
+                    <resolution>{{ $torrent['resolution']['name'] ?? 'No Res' }}</resolution>
+                    <size> {{ App\Helpers\StringHelper::formatBytes($torrent['size'], 2) }}</size>
+                    <imdb>@if (($torrent['category']['movie_meta'] || $torrent['category']['tv_meta']) && $torrent['imdb'] != 0)
+                                https://anon.to?http://www.imdb.com/title/tt{{ \str_pad((string) $torrent['imdb'], \max(\strlen((string) $torrent['imdb']), 7), '0', STR_PAD_LEFT) }}
+                           @endif
+                    </imdb>
+                    <poster>
+				@php
+				$poster='https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg';
+				if ($torrent['category']['movie_meta'] && $torrent['tmdb'] != 0)
+					$poster = tmdb_image('poster_big', $meta->poster);
+				if ($torrent['category']['tv_meta'] && $torrent['tmdb'] != 0)
+                                	$poster = tmdb_image('poster_big', $meta->poster);
+				if (!$torrent['category']['tv_meta'] && !$torrent['category']['movie_meta'] )
+					if(file_exists(public_path().'/files/img/torrent-cover_' . $torrent['id'] . '.jpg'))
+						$poster = url('files/img/torrent-cover_' . $torrent['id'] . '.jpg');
+				@endphp
+				{{$poster}}
+		    </poster>
+                    
+                    <tmdb>@if ($torrent['category']['movie_meta'] && $torrent['tmdb'] != 0)
+                                https://anon.to?https://www.themoviedb.org/movie/{{ $torrent['tmdb'] }}
+                            @elseif ($torrent['category']['tv_meta'] && $torrent['tmdb'] != 0)
+                                https://anon.to?https://www.themoviedb.org/tv/{{ $torrent['tmdb'] }}
+                            @endif</tmdb>
+                    
+                    <link>{{ route('torrents.show', ['id' => $torrent['id']]) }}</link>
                     <guid>{{ $torrent['id'] }}</guid>
                     <description>
                         <![CDATA[<p>
+                            {{$poster}} 
                             <strong>Name</strong>: {{ $torrent['name'] }}<br>
                             <strong>Category</strong>: {{ $torrent['category']['name'] }}<br>
                             <strong>Type</strong>: {{ $torrent['type']['name'] }}<br>
@@ -74,6 +114,11 @@
                         @endif
                     </dc:creator>
                     <pubDate>{{ \Illuminate\Support\Carbon::createFromTimestampUTC($torrent['created_at'])->toRssString() }}</pubDate>
+                    <enclosure
+                            url="{{ route('torrent.download.rsskey', ['id' => $torrent['id'], 'rsskey' => $user->rsskey ]) }}"
+                            type="application/x-bittorrent"
+                            length="39399"
+                    />
                 </item>
             @endforeach
         @endif
